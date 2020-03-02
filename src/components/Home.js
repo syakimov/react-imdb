ba4589fab899f1647d8d4d503ab97f47a6e56c66
Stefan Yakimov <stefan.yakimov@receipt-bank.com>
2020-03-02 12:22:30 +0200

    Fetch movies from the IMDB API with custom hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { API_URL, API_KEY, API_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../config';
 
 // import components
 import HeroImage from './elements/HeroImage';
@@ -8,15 +9,23 @@ import MovieThumb from './elements/MovieThumb';
 import LoadMoreBtn from './elements/LoadMoreBtn';
 import Spinner from './elements/Spinner';
 
-const Home = () => (
-  <>
-    <HeroImage/>
-    <SearchBar/>
-    <Grid/>
-    <MovieThumb/>
-    <Spinner/>
-    <LoadMoreBtn/>
-  </>
-)
+// Custom hook
+import {useHomeFetch} from '../components/hooks/useHomeFetch';
+
+const Home = () => {
+  const [{state, loading, error}, fetchMovies] = useHomeFetch();
+  console.log(state);
+
+  return (
+    <>
+      <HeroImage/>
+      <SearchBar/>
+      <Grid/>
+      <MovieThumb/>
+      <Spinner/>
+      <LoadMoreBtn/>
+    </>
+  )
+}
 
 export default Home;
---
+++ src/components/hooks/useHomeFetch.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { API_URL, API_KEY } from '../../config';
+
+export const useHomeFetch = () => {
+  const [state, setState] = useState({movies: []});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  // async says this is an async function
+  const fetchMovies = async endpoint => {
+    setError(false);
+    setLoading(false);
+
+    try {
+      // wait for the call to finish two times
+      // First when we fetch the data
+      // Second when we parse the response (.json is async)
+      const result = await (await fetch(endpoint)).json();
+      setState(prev => ({
+        ...prev,
+        movies: [...result.results],
+        heroImage: prev.heroImage || result.results[0],
+        currentPage: result.page,
+        totalPages: result.total_pages,
+      }))
+    } catch(error) {
+      setError(true);
+      console.log(error);
+    }
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
+  }, [])
+
+  return [{state, loading, error}, fetchMovies];
+};
