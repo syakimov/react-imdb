2a5a833a932f61f83568a47c629aad63a79956df
Stefan Yakimov <stefan.yakimov@receipt-bank.com>
2020-03-07 20:31:26 +0200

    Implement SearchBar part I - Searchbar as Controlled Component

--- src/components/elements/SearchBar.js
+++ src/components/elements/SearchBar.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import FontAwesome from 'react-fontawesome';
 
-const SearchBar = () => <div>SearchBar</div>
+import {
+  StyledSearchBar,
+  StyledSearchBarContent,
+} from '../styles/StyledSearchBar';
+
+const SearchBar = ({callback}) => {
+  const [state, setState] = useState('');
+
+  const doSearch = event => {
+    const { value } = event.target;
+    setState(value);
+  };
+
+  return (
+    <StyledSearchBar>
+      <StyledSearchBarContent>
+        <FontAwesome className="fa-search" name="search" size="2x" />
+        <input
+          type="text"
+          placeholder="Search Movie"
+          onChange={doSearch}
+          value={state}
+        />
+      </StyledSearchBarContent>
+    </StyledSearchBar>
+  );
+};
 
 export default SearchBar;
